78ff52fce58011023694045421a6f8b662a95417
Shahista <patelshahista96@gmail.com>
2024-03-03 06:52:25 +0800

    Initial Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,32 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule } from '@angular/material/toolbar';
+
+import { MatButtonModule } from '@angular/material/button';
+import { MatMenuModule } from '@angular/material/menu';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatButtonModule,
+MatMenuModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/navbar/navbar.component.ts
@@ -0,0 +1,60 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+
+interface MenuItem {
+  name: string;
+  subItems: string[];
+  showSubMenu: boolean;
+}
+
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.scss']
+})
+export class NavbarComponent implements OnInit {
+
+  menuItems = [
+    { name: 'Home', subItems: [] },
+    { name: 'Our Exemplars', subItems: [] },
+    { name: 'Madrasah', subItems: ['Our Courses', 'Traditional Islamic Education'],  showSubMenu: false },
+    { name: 'Women\'s Wellness Center', subItems: ['Submenu 3', 'Submenu 4'] ,  showSubMenu: false},
+    { name: 'FAQs', subItems: ['Submenu 1', 'Submenu 2'], showSubMenu: false },
+    { name: 'Contact Us', subItems: ['Submenu 3', 'Submenu 4'], showSubMenu: false },
+    // Add more menu items with sub-items as needed
+  ];
+
+
+
+  
+
+  htmlContent: string = '';
+
+
+  showDropdown: boolean[] = [];
+
+
+  toggleDropdown(menuItem:any) {
+    menuItem.showSubMenu = !menuItem.showSubMenu;
+  }
+
+  constructor(private http: HttpClient) { 
+    this.menuItems.forEach(() => this.showDropdown.push(false));
+  }
+
+  ngOnInit(): void {
+    this.http.get('assets/Html/faqs_si.html', { responseType: 'text' })
+    .subscribe((data: string) => {
+      this.htmlContent = data; // Assign fetched HTML content to a variable
+    });
+  }
+
+
+  loadHomePage(){
+    this.http.get('assets/Html/home.html', { responseType: 'text' })
+    .subscribe((data: string) => {
+      this.htmlContent = data; // Assign fetched HTML content to a variable
+    });
+  }
+
+}
