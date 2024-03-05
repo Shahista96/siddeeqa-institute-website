c1a8d561e36dcb61a54f313809d0caf9d770d115
DESKTOP-27C9PS5\Shahrukh <shahrukhpatel353@gmail.com>
2024-03-05 17:53:51 +0530

    Build modified

--- src/app/contents/home/home.component.ts
+++ src/app/contents/home/home.component.ts
@@ -10,7 +10,7 @@ export class HomeComponent {
   responsiveOptions: any[] | undefined;
 
   images: String [] = ["../../../assets/Images/Carousel/1681766172552.webp","../../../assets/Images/Carousel/Islamic finance Teaser 2023.webp","../../../assets/Images/Carousel/photo_2024-03-01_12-12-49.webp","../../../assets/Images/Carousel/1681766172535.webp","../../../assets/Images/Carousel/1681766172544.webp","../../../assets/Images/Carousel/Teacher.webp","../../../assets/Images/Carousel/journey to light.webp","../../../assets/Images/Carousel/photo_2024-03-01_12-12-36.webp"]
-
+  links: String [] = ["../../../assets/Files/Carousel/5Yr-EN.pdf","../../../assets/Files/Carousel/Islamic finance Teaser 2023.webp","../../../assets/Files/Carousel/photo_2024-03-01_12-12-49.webp","../../../assets/Files/Carousel/1681766172535.webp","../../../assets/Files/Carousel/1681766172544.webp","../../../assets/Files/Carousel/Teacher.webp","../../../assets/Files/Carousel/journey to light.webp","../../../assets/Files/Carousel/photo_2024-03-01_12-12-36.webp"];
   ngOnInit() {
 
     this.responsiveOptions = [
