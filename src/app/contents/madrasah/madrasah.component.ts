bdf41962b271fe5cdc3d0ed64fa341c67b08b8f1
Shahista <patelshahista96@gmail.com>
2024-03-08 11:43:34 +0800

    Fiqh Resources

--- src/app/contents/madrasah/madrasah.component.ts
+++ src/app/contents/madrasah/madrasah.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input } from '@angular/core';
 import { MatAccordion } from "@angular/material/expansion";
 
+
 @Component({
   selector: 'app-madrasah',
   templateUrl: './madrasah.component.html',
@@ -9,5 +10,14 @@ import { MatAccordion } from "@angular/material/expansion";
 export class MadrasahComponent {
 
   @Input() subMenu: string = '';
+
+  openPdf(pdf:string): void {
+    // Navigate to PDF file URL in the browser
+    if(pdf === 'Tarawih'){
+      window.open('../../../assets/PDFs/TarawihPrayer.pdf', '_blank'); // Change 'your-pdf-file.pdf' to your actual PDF file name
+    }else if(pdf === 'Zakat'){
+      window.open('../../../assets/PDFs/Zakat.pdf', '_blank'); // Change 'your-pdf-file.pdf' to your actual PDF file name
+    }
+  }
 
 }
