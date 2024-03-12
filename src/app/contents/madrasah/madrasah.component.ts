6be5ef3a1d6a7b679d26f12a811519dc2074742e
Shahista <patelshahista96@gmail.com>
2024-03-12 10:56:53 +0800

    Madrasah - all sub menus translated to Urdu

--- src/app/contents/madrasah/madrasah.component.ts
+++ src/app/contents/madrasah/madrasah.component.ts
@@ -1,6 +1,4 @@
 import { Component, Input } from '@angular/core';
-import { MatAccordion } from "@angular/material/expansion";
-
 
 @Component({
   selector: 'app-madrasah',
@@ -32,12 +30,17 @@ export class MadrasahComponent {
   short_images: String [] = ["../../../assets/Images/madrasah/carrousel_1.webp","../../../assets/Images/madrasah/carrousel_2.webp","../../../assets/Images/madrasah/carrousel_3.webp"]
   short_links: String [] = ["../../../assets/PDFs/madrasah/carrousel_1.pdf","../../../assets/PDFs/madrasah/carrousel_2.pdf","../../../assets/PDFs/madrasah/carrousel_3.pdf"];
   post_images: String [] = ["../../../assets/Images/madrasah/carrousel_1.webp","../../../assets/Images/madrasah/carrousel_2.webp","../../../assets/Images/madrasah/carrousel_3.webp"]
+  
   openPdf(pdf:string): void {
     // Navigate to PDF file URL in the browser
     if(pdf === 'Tarawih'){
       window.open('../../../assets/PDFs/TarawihPrayer.pdf', '_blank'); // Change 'your-pdf-file.pdf' to your actual PDF file name
     }else if(pdf === 'Zakat'){
       window.open('../../../assets/PDFs/Zakat.pdf', '_blank'); // Change 'your-pdf-file.pdf' to your actual PDF file name
+    }else if(pdf === 'Tarawih_UR'){
+      window.open('../../../assets/PDFs/Tarawih_UR.pdf', '_blank'); // Change 'your-pdf-file.pdf' to your actual PDF file name
+    }else if(pdf === 'Zakat_UR'){
+      window.open('../../../assets/PDFs/Zakaat_UR.pdf', '_blank'); // Change 'your-pdf-file.pdf' to your actual PDF file name
     }
   }
 
